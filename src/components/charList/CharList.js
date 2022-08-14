aecd8b79f91e254f822115dc8cb7a6489e3cc1ae
Artyom <saaworking@gmail.com>
2022-08-14 20:28:09 +0300

    chars pagination

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -3,11 +3,16 @@ import { Component } from 'react';
 import MarvelService from '../../services/MarvelServices';
 import Spinner from '../spinner/Spinner';
 import Error from '../error/Error';
+import Loading from '../loading/Loading';
 class CharList extends Component {
+
 	state = {
 		chars: [],
 		loading: true,
-		error: false
+		error: false,
+		newItemLoading: false,
+		offset: 1550,
+		charEnded: false
 	}
 
 
@@ -17,18 +22,29 @@ class CharList extends Component {
 		this.updateChar()
 	}
 
-	onCharsLoaded = (chars) => {
-		this.setState({ chars, loading: false })
+	onCharsLoaded = (nextChars) => {
+		let ended = false;
+		if (nextChars.length < 9) {
+			ended = true;
+		}
+
+		this.setState(({ chars, newItemLoading, offset }) => ({
+			chars: [...chars, ...nextChars],
+			loading: false,
+			newItemLoading: false,
+			offset: offset + 9,
+			charEnded: ended
+		}))
 	}
 
 	onCharsLoading = () => {
-		this.setState({ loading: true });
+		this.setState({ newItemLoading: true });
 	}
 
 	updateChar = () => {
 		this.onCharsLoading();
 		this.marvelService
-			.getAllCharacters()
+			.getAllCharacters(this.state.offset)
 			.then(this.onCharsLoaded)
 			.catch(this.onError)
 	}
@@ -37,42 +53,53 @@ class CharList extends Component {
 		this.setState({ loading: false, error: true })
 	}
 
+	renderButton = () => {
+		return (
+			<button className="button button__main button__long"
+				onClick={this.updateChar}>
+				<div className="inner">load more</div>
+			</button>
+		)
+	}
+
+	renderList = (chars) => {
+		const charsItem = chars.map(item => {
+			const { thumbnail, alt, name, id } = item;
+			let thumbnailStyle = { objectFit: 'cover' };
+
+			if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+				thumbnailStyle.objectFit = 'contain';
+			}
+			return (
+				<li className="char__item" onClick={() => this.props.onCharSelected(id)} key={id}>
+					<img src={thumbnail} alt={alt} style={thumbnailStyle} />
+					<div className="char__name">{name}</div>
+				</li>
+			)
+		});
+		return (
+			<ul className="char__grid">
+				{charsItem}
+			</ul>
+		)
+	}
+
 	render() {
-		const { chars, loading, error } = this.state;
-		const { onCharSelected } = this.props;
+		const { chars, loading, error, newItemLoading, charEnded } = this.state;
 		const loadStatus = loading ? <Spinner /> : null;
 		const errorStatus = error ? <Error /> : null;
-		const visible = (loadStatus || errorStatus) ? null : chars.map(item => <CharItem {...item} key={item.id} onCharSelected={() => onCharSelected(item.id)} />);
+		const visible = (loadStatus || errorStatus) ? null : this.renderList(chars);
+		const loadButton = newItemLoading ? <Loading /> : this.renderButton()
 
 		return (
 			<div className="char__list">
-				<ul className="char__grid">
-					{loadStatus}
-					{errorStatus}
-					{visible}
-				</ul>
-				<button className="button button__main button__long">
-					<div className="inner">load more</div>
-				</button>
+				{loadStatus}
+				{errorStatus}
+				{visible}
+				{!charEnded ? loadButton : null}
 			</div>
 		)
 	}
-}
-
-
-const CharItem = (props) => {
-	const { thumbnail, alt, name, onCharSelected } = props;
-	let thumbnailStyle = { objectFit: 'cover' };
-
-	if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
-		thumbnailStyle.objectFit = 'contain';
-	}
-	return (
-		<li className="char__item" onClick={onCharSelected}>
-			<img src={thumbnail} alt={alt} style={thumbnailStyle} />
-			<div className="char__name">{name}</div>
-		</li>
-	)
 }
 
 export default CharList;
---
+++ src/components/loading/Loading.js
@@ -0,0 +1,9 @@
+import loading from './loading.gif'
+
+const Loading = () => {
+	return (
+		<img src={loading} alt="spinner" style={{ display: 'block', padding: '19px 0px 0px', margin: '0 auto' }} />
+	)
+}
+
+export default Loading;
